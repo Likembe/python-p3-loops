eee624ecb309f41d328bfa673d62b139fbaaa51c
Alpha <alpha.likembe@student.moringaschool.com>
2023-09-30 06:16:41 +0300

    trials

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i>0:
+        print(i)
+        i-=1
+    print("Happy New Year!")
+
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_integers = []
+    for num in int_list:
+        square_integers.append(num ** 2)
+    return square_integers
+
+#Usage Example
+int_list = [1,2,3,4,5]
+result = square_integers(int_list)
+print(result)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 ==0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz()
